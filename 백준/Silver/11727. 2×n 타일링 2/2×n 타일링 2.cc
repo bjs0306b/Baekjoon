972298d8f448e8befca9c76886725ee6899686d8
bjs0306b <144401380+bjs0306b@users.noreply.github.com>
2024-02-03 20:20:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin >> n;
+    long long arr[n+1]; arr[1]=1; arr[2]=3;
+    for(int i=3;i<n+1;i++){
+        arr[i] = (arr[i-1] + 2*arr[i-2])%10007;
+    }
+    cout << arr[n]%10007;
+    return 0;
+}
