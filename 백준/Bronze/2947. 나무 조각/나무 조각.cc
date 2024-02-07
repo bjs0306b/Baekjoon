383e8ffefdb85441da219fe49e2337fd6c6a2024
bjs0306b <144401380+bjs0306b@users.noreply.github.com>
2024-02-07 16:08:25 +0900

    [Bronze I] Title: 나무 조각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int arr[5]; for(int i=0;i<5;i++) cin >>arr[i];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                for(int k=0;k<5;k++) cout << arr[k] << " ";
+                cout << "\n";
+            }
+        }
+    }
+    
+    
+    return 0;
+}
