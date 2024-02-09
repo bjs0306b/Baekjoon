13de9f9388d97d067401046f016ca60a560b668b
bjs0306b <144401380+bjs0306b@users.noreply.github.com>
2024-02-09 14:58:05 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory.h>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    string s; cin >> s;
+    cout << ":fan::fan::fan:\n:fan::" << s << "::fan:\n:fan::fan::fan:";
+    
+    
+    return 0;
+}
