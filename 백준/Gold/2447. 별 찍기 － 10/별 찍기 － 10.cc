dbd08e626ae7d0a1abec74b0d85a2b89629b1561
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-02-22 20:12:22 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 256 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory.h>
+#include <vector>
+using namespace std;
+
+void star(int i, int j, int num)
+{
+    if((i / num)%3 == 1 && (j / num)%3 == 1) {
+        cout << ' ';
+    }
+    else
+    {
+        if(num / 3 == 0)
+            cout <<'*';
+        else
+            star(i,j,num/3);
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int num;
+    cin >> num;
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < num; j++)
+            star(i,j,num);
+        cout << '\n';
+    }
+    
+    return 0;
+}
