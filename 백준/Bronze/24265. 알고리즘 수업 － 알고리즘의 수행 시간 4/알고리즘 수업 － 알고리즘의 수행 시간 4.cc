63dac4dff733dd1a2c54bc9209f35c710e5bbe1d
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-02-23 17:05:43 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory.h>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    long long a; cin >> a;
+    cout << a*(a-1)/2 << "\n2";
+    
+    return 0;
+}
