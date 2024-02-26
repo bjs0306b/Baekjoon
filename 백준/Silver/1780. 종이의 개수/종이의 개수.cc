0411c81726dec44f2c357cc69ebe6fb7917943cc
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-02-26 20:02:15 +0900

    [Silver II] Title: 종이의 개수, Time: 396 ms, Memory: 20928 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,71 +1,56 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <memory.h>
-#include <vector>
-#include <set>
 #include <cmath>
 using namespace std;
-int arr[2187][2187];
-int ans[3];
-void dfs(int a, int b, int s)
-{
-    bool check = true;
-    for (int i = (a / s) * s; i < (a / s) * s + s; i++)
-    {
-        for (int j = (b / s) * s; j < (b / s) * s + s; j++)
-        {
-            if (arr[i][j] != arr[a][b]){
-                check = false;
+int n;
+int one = 0;
+int m_one = 0;
+int ze = 0;
+int map[2200][2200];
+void papper(int x, int y, int size){
+    bool same = true;
+    for (int i = y; i < y+size;i++){ //각 분면 탐색
+        for (int j = x; j < x + size;j++){
+            if(map[i][j] != map[y][x]){
+                same = false; // 원소 하나라도 다르면 false
                 break;
             }
         }
-        if(!check) break;
+        if(!same){
+            break;
+        }
     }
-    if (check)
-    {
-        dfs(a, b, 3 * s);
-    }
-    else
-    {
-        if (arr[a][b] == -1)
-            ans[0]++;
-        else if (arr[a][b] == 0)
-            ans[1]++;
-        else if (arr[a][b] == 1)
-            ans[2]++;
-
-        s /= 3;
-        for (int i = (a / s) * s; i < (a / s) * s + s; i++)
-        {
-            for (int j = (b / s) * s; j < (b / s) * s + s; j++)
-            {
-                arr[i][j] = 2;
-            }
+    if(!same){
+        papper(x, y, size / 3); // 1번
+        papper(x+size/3, y, size / 3); // 2번
+        papper(x+size/3+size/3, y, size / 3); // 3번
+        papper(x, y+size/3, size / 3); // 4번
+        papper(x, y+size/3+size/3, size / 3); // 7번
+        papper(x+size/3, y+size/3, size / 3); // 5번
+        papper(x+size/3+size/3, y+size/3+size/3, size / 3); // 9번
+        papper(x+size/3, y+size/3+size/3, size / 3); // 8번
+        papper(x+size/3+size/3, y+size/3, size / 3); // 6번
+    }else{
+        if(map[y][x] == 1){
+            one++; // 1인 경우
+        }else if(map[y][x] == -1){
+            m_one++; // -1인 경우
+        }else{
+            ze++; // 0인 경우
         }
     }
 }
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n;
+ 
+int main(){
+    ios::sync_with_stdio(false);
     cin >> n;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> arr[i][j];
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[i][j] != 2)
-            {
-                dfs(i, j, 1);
-            }
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < n;j++){
+            cin >> map[i][j];
         }
     }
-    for (int i = 0; i < 3; i++)
-        cout << ans[i] << "\n";
+    papper(0, 0, n);
+    cout << m_one << '\n';
+    cout << ze << '\n';
+    cout << one << '\n';
     return 0;
-}
+}// 28min
