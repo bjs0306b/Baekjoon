237afe7717e59f2fcfb54be71540f4a0bd5a61d4
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-03-13 15:40:02 +0900

    [Bronze II] Title: 부분 문자열, Time: 16 ms, Memory: 10012 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
@@ -1,48 +1,46 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <memory.h>
 #include <vector>
-#include <deque>
-#include <set>
-#include <cmath>
-#include <string.h>
 using namespace std;
 
-#define MAX 1000001
-int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    char a[MAX],b[MAX]; scanf("%s %s", a, b);
-    char* ans = strstr(a,b);
-    if(ans == NULL) cout << 0;
-    else cout << 1;
-    
-    return 0;
+
+vector<int> makeTable(string p) {
+	vector<int> table(p.length());
+	int j = 0;
+	for (int i = 1; i < p.length(); i++) {
+		while (p[i] != p[j] && j > 0) {
+			j = table[--j];
+		}
+		if (p[i] == p[j]) {
+			table[i] = ++j;
+		}
+	}
+	return table;
 }
 
-// 라빈-카프 알고리즘 (Rabin-Karp)인데 string 길이가 너무 길어지면 overflow를 막을 수가 없나보다..
-// int main(){
-//     ios_base::sync_with_stdio(0);cin.tie(0);
-//     string a,b; cin >> a >> b;
-//     int hash_b =0;
-//     int size_b = b.size();
-//     for(int i = size_b-1; i>=0; i--){
-//         hash_b += pow(size_b, i)*((int)b[size_b-1-i]);
-//         hash_b %= 65536;
-//     }
-//     int hash_a = 0;
-//     int size_a = a.size();
-//     for(int i = size_b-1; i>=0; i--){
-//         hash_a += pow(size_b, i)*((int)a[size_b-1-i]);
-//         hash_a %= 65536;
-//     }
-//     if(hash_a == hash_b){cout << 1; return 0;}
+int kmp(string s, string p, vector<int> &table) {
+	int j = 0;
+	for (int i = 0; i < s.length(); i++) {
+		while (s[i] != p[j] && j > 0) {
+			j = table[--j];
+		}
+		if (s[i] == p[j]) {
+			if (++j == p.length()) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
 
-//     for(int i=size_b;i<size_a;i++){
-//         hash_a = size_b*(hash_a - (int)a[i-size_b]*pow(size_b, size_b-1)) + (int)a[i];
-//         if(hash_a == hash_b) {cout << 1; return 0;}
-//     }
-//     cout << 0;
-    
-//     return 0;
-// }
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string s, p;
+	cin >> s >> p;
+
+	vector<int> table = makeTable(p);
+	cout << kmp(s, p, table);
+	return 0;
+}
