0986d86fdb9f51ff3a34498c9b3d70b6e8b4ef31
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-03-18 16:55:32 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 2020 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory.h>
+#include <vector>
+#include <deque>
+#include <set>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    float a,b; cin >> a >> b;
+    printf("%f", a/b);
+    
+    
+    return 0;
+}
