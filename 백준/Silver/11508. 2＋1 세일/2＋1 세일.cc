98eba21f9b13275c918b5e74149f81f30a088a3d
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-03-21 18:14:55 +0900

    [Silver IV] Title: 2+1 세일, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory.h>
+#include <vector>
+#include <deque>
+#include <set>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n; cin >> n;
+    int arr[n]; for(int i=0;i<n;i++) cin >> arr[i];
+    sort(arr, arr+n, greater<int>());
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(i%3 != 2) sum += arr[i];
+    }
+    cout << sum;
+    return 0;
+}
+
