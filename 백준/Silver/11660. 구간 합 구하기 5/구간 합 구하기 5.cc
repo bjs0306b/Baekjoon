c686c901ee43af6fd175fc044413902c83098b82
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-03-27 14:02:40 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 128 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,38 +1,33 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <memory.h>
-#include <vector>
-#include <deque>
-#include <set>
-#include <cmath>
+
 using namespace std;
+
+int arr[1025][1025],dp[1025][1025];
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int a, b;
-    cin >> a >> b;
-    int arr[a + 1][a + 1];
-    memset(arr, 0, sizeof(arr));
-    for (int i = 1; i <= a; i++)
-        for (int j = 1; j <= a; j++)
-        {
-            cin >> arr[i][j];
-            arr[i][j] += arr[i][j - 1];
-        }
-    for (int i = 0; i < b; i++)
-    {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        int ans = 0;
-        for (int j = x1; j <= x2; j++)
-        {
-            ans += (arr[j][y2] - arr[j][y1-1]);
-        }
-        cout << ans << "\n";
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cin>>arr[i][j];
+            dp[i][j] = dp[i-1][j]+dp[i][j-1] - dp[i-1][j-1]+arr[i][j];
+        }
+    }
+    int x1,y1,x2,y2;
+    int ans;
+    for(int i=0;i<m;i++)
+    {
+        cin>>x1>>y1;
+        cin>>x2>>y2;
+        ans = dp[x2][y2] - dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1];
+        cout<<ans<<'\n';
+    }
     return 0;
 }
