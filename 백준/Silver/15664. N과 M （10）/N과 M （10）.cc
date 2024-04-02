4197e549866fed2e85283a7507c2ba1e57fcb201
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-04-02 16:41:32 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -11,22 +11,23 @@
 #include <stack>
 #include <cmath>
 using namespace std;
+
 int m,n;
 vector<int> v;
-set<vector<int>> ss;
-int arr[9], before[9];
+int arr[9];
 bool visited[9];
 void dfs(int a, int s){
     if(n==a){
-        vector<int> temp;
-        for(int i=0;i<n;i++) temp.push_back(arr[i]);
-        ss.insert(temp);
+        for(int i=0;i<n;i++) cout << arr[i] << ' ';
+        cout << '\n';
     }
     else{
+        int temp = 0;
         for(int i=s;i<m;i++){
-            if(!visited[i]){
+            if(!visited[i] && v[i] != temp){
                 visited[i] = true;
                 arr[a] = v[i];
+                temp = arr[a];
                 dfs(a+1,i+1);
                 visited[i] = false;
             }
@@ -39,9 +40,40 @@ int main(){
     for(int i=0;i<m;i++){int num; cin >> num; v.push_back(num);}
     sort(v.begin(),v.end());
     dfs(0,0);
-    for(auto k : ss){
-        for(auto l : k) cout << l << ' ';
-        cout << '\n';
-    }
     return 0;
 }
+
+// int m,n;
+// vector<int> v;
+// set<vector<int>> ss;
+// int arr[9], before[9];
+// bool visited[9];
+// void dfs(int a, int s){
+//     if(n==a){
+//         vector<int> temp;
+//         for(int i=0;i<n;i++) temp.push_back(arr[i]);
+//         ss.insert(temp);
+//     }
+//     else{
+//         for(int i=s;i<m;i++){
+//             if(!visited[i]){
+//                 visited[i] = true;
+//                 arr[a] = v[i];
+//                 dfs(a+1,i+1);
+//                 visited[i] = false;
+//             }
+//         }
+//     }
+// }
+// int main(){
+//     ios_base::sync_with_stdio(0);cin.tie(0);
+//     cin >> m >> n;
+//     for(int i=0;i<m;i++){int num; cin >> num; v.push_back(num);}
+//     sort(v.begin(),v.end());
+//     dfs(0,0);
+//     for(auto k : ss){
+//         for(auto l : k) cout << l << ' ';
+//         cout << '\n';
+//     }
+//     return 0;
+// }
