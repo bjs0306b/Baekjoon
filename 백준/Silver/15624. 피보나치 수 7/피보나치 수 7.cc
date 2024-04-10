fe13370286c7f25d7b370524eb7fd7abe2e517af
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-04-10 13:55:20 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 8 ms, Memory: 9832 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -12,21 +12,15 @@
 #include <cmath>
 using namespace std;
 
+long long arr[1000001];
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
-    int n; cin >> n;
-    if(n==0) cout << 0;
-    if(n<=2) cout << 1;
-    int a = 1;
-    int b = 1;
-    int c = a+b;
-
+    long long n; cin >> n;
+    arr[1] = 1;
+    arr[2] = 1;
     for(int i=3;i<=n;i++){
-        a = b;
-        b = c;
-        c = (a+b)%1000000007;
+        arr[i] = (arr[i-1] + arr[i-2]) % 1000000007;
     }
-    cout << b;
-    
+    cout << arr[n];
     return 0;
 }
