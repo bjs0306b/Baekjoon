0177b5411308c047f2ce2d773566767ac55d435b
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-05-02 20:18:26 +0900

    [Silver III] Title: 칸토어 집합, Time: 532 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory.h>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+bool func(int n, int k){
+    if(n < 3){
+        if(n == 1) return false;
+        else return true;
+    }
+    else{
+        if(n / (int)pow(3,k-1) == 1 ) return false;
+        else{
+            int temp = pow(3,k-1);
+            return func(n%temp, k-1);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    
+    int n; 
+    while(cin >> n){
+        for(int i=0;i<pow(3,n);i++){
+            if(func(i,n)) cout << "-";
+            else cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
