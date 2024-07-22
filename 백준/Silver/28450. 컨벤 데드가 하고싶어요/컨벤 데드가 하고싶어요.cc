1b75e54c3b507aff7dea475d47088655d3c798cf
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-07-22 20:53:38 +0900

    [Silver II] Title: 컨벤 데드가 하고싶어요, Time: 188 ms, Memory: 13184 KB -BaekjoonHub

--- 백준/Silver/28450. 컨벤 데드가 하고싶어요/컨벤 데드가 하고싶어요.cc
+++ 백준/Silver/28450. 컨벤 데드가 하고싶어요/컨벤 데드가 하고싶어요.cc
@@ -1,66 +1,78 @@
-#include <bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int a,b;
+// long long arr[1001][1001], arr2[1001][1001];
+// bool visited[1001][1001];
+// long long h;
+
+// void dfs(int x, int y, long long sum){
+
+//     arr2[x][y] = sum;
+//     visited[x][y] = true;
+//     if(x+1 < a && (!visited[x+1][y] || arr2[x+1][y] > arr2[x][y] + arr[x+1][y])) dfs(x+1, y, arr2[x][y] + arr[x+1][y]);
+//     if(y+1 < b && (!visited[x][y+1] || arr2[x][y+1] > arr2[x][y] + arr[x][y+1])) dfs(x, y+1, arr2[x][y] + arr[x][y+1]);
+// }
+
+// int main(){
+//     ios_base::sync_with_stdio(0);cin.tie(0);
+//     cin >> a >> b;
+
+//     for(int i=0;i<a;i++){
+//         for(int j=0;j<b;j++){
+//             cin >> arr[i][j];
+//         }
+//     }
+
+//     cin >> h;
+
+//     dfs(0,0,arr[0][0]);
+
+//     if(arr2[a-1][b-1] > h) cout << "NO";
+//     else cout << "YES\n" << arr2[a-1][b-1];
+    
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int a, b;
-long long arr[1001][1001], arr2[1001][1001];
+int a,b,h, arr[1001][1001];
 bool visited[1001][1001];
-long long h;
 
-struct Node {
-    int x, y;
-    long long cost;
-    bool operator>(const Node &other) const {
-        return cost > other.cost;
+struct node{
+    int x,y;
+    long long sum;
+
+    bool operator>(const node other)const{
+        return sum > other.sum;
     }
 };
 
-void dijkstra() {
-    priority_queue<Node, vector<Node>, greater<Node>> pq;
-    pq.push({0, 0, arr[0][0]});
-    arr2[0][0] = arr[0][0];
-
-    while (!pq.empty()) {
-        Node curr = pq.top();
-        pq.pop();
-        
-        int x = curr.x;
-        int y = curr.y;
-        long long cost = curr.cost;
-
-        if (visited[x][y]) continue;
-        visited[x][y] = true;
-
-        if (x+1 < a && (!visited[x+1][y] && arr2[x+1][y] > cost + arr[x+1][y])) {
-            arr2[x+1][y] = cost + arr[x+1][y];
-            pq.push({x+1, y, arr2[x+1][y]});
-        }
-
-        if (y+1 < b && (!visited[x][y+1] && arr2[x][y+1] > cost + arr[x][y+1])) {
-            arr2[x][y+1] = cost + arr[x][y+1];
-            pq.push({x, y+1, arr2[x][y+1]});
-        }
-    }
-}
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
     
     cin >> a >> b;
-
-    for(int i = 0; i < a; i++) {
-        for(int j = 0; j < b; j++) {
-            cin >> arr[i][j];
-            arr2[i][j] = LLONG_MAX;
-        }
-    }
-
+    for(int i=0;i<a;i++) for(int j=0;j<b;j++)cin >> arr[i][j];
     cin >> h;
+    long long best;
+    priority_queue<node, vector<node>, greater<node>> pq;
+    pq.push({0,0,arr[0][0]});
+    while(!pq.empty()){
+        node temp = pq.top();
+        pq.pop();
+        int x = temp.x;
+        int y = temp.y;
+        int sum = temp.sum;
+        if(x == a-1 && y == b-1){best = sum; break;}
+        if(visited[x][y]) continue;
+        visited[x][y] = true;
 
-    dijkstra();
+        if(x+1 < a && !visited[x+1][y]) pq.push({x+1, y, sum + arr[x+1][y]});
+        if(y+1 < b && !visited[x][y+1]) pq.push({x, y+1, sum + arr[x][y+1]});
+    }   
+    if(best > h) cout << "NO" ;
+    else cout << "YES\n" << best;
 
-    if (arr2[a-1][b-1] > h) cout << "NO";
-    else cout << "YES\n" << arr2[a-1][b-1];
-    
     return 0;
 }
