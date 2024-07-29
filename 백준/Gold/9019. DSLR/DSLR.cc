f3d1425b5018db177d1a7c42ff070911d0fb2c99
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-07-29 18:39:58 +0900

    [Gold IV] Title: DSLR, Time: 3684 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,63 +1,81 @@
-#include<bits/stdc++.h>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <cstring>
+
 using namespace std;
 
-struct tmp{
-    int num;
-    string str;
-};
+int a, b;
+bool visited[10000];
 
-int left(int n){
-    int temp = n / 1000;
-    n = (n % 1000) * 10 + temp;
-    return n;
-}
+void bfs()
+{
+    queue<pair<int, string>> q;
+    q.push(make_pair(a, ""));
+    visited[a] = true;
 
-int right(int n){
-    int temp = n % 10;
-    n -= temp;
-    n /= 10;
-    n += temp * 1000;
-    return n;
-}
+    while (!q.empty())
+    {
+        int cur_num = q.front().first;
+        string cur_op = q.front().second;
+        q.pop();
 
+        if (cur_num == b)
+        {
+            cout << cur_op << '\n';
+            return;
+        }
 
-int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    int n; cin >> n;
-    for(int i=0;i<n;i++){
-        bool arr[10000]; memset(arr, 0, 10000);
-        queue<tmp> q;
-        int a,b; cin >> a >> b;
-        q.push({a,""});
-        arr[a] = 1;
-        for(;;){
-            tmp temp = q.front();
-            q.pop();
-            int qnum = temp.num;
+        int D, S, L, R, temp;
+        // D 연산
+        D = (cur_num * 2) % 10000;
+        if (!visited[D])
+        {
+            visited[D] = true;
+            q.push(make_pair(D, cur_op + "D"));
+        }
 
-            if(qnum == b){cout << temp.str << "\n"; break;}
+        // S 연산
+        S = cur_num - 1 < 0 ? 9999 : cur_num - 1;
+        if (!visited[S])
+        {
+            visited[S] = true;
+            q.push(make_pair(S, cur_op + "S"));
+        }
 
-            if(!arr[(qnum+9999)%10000]){
-                arr[(qnum+9999)%10000] = 1;
-                q.push({(qnum+9999)%10000, temp.str + 'S'});
-            }
+        // L 연산
+        L = (cur_num % 1000) * 10 + (cur_num / 1000);
+        if (!visited[L])
+        {
+            visited[L] = true;
+            q.push(make_pair(L, cur_op + "L"));
+        }
 
-            if(!arr[left(qnum)]){
-                arr[left(qnum)] = 1;
-                q.push({left(qnum), temp.str + 'L'});
-            }
-
-            if(!arr[right(qnum)]){
-                arr[right(qnum)] = 1;
-                q.push({right(qnum), temp.str + 'R'});
-            }
-
-            if(!arr[(qnum*2)%10000]){
-                arr[(qnum*2)%10000] = 1;
-                q.push({(qnum*2)%10000, temp.str + 'D'});
-            }
+        // R 연산
+        R = cur_num / 10 + (cur_num % 10) * 1000;
+        if (!visited[R])
+        {
+            visited[R] = true;
+            q.push(make_pair(R, cur_op + "R"));
         }
     }
-    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        cin >> a >> b;
+        memset(visited, false, sizeof(visited)); // 초기화
+        bfs();
+    }
+
     return 0;
 }
