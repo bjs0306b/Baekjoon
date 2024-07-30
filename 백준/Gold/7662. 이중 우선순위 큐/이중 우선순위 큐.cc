f6adc3e61f9a40ee0861bb06c46ae82f2ceceef7
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-07-30 18:57:49 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1740 ms, Memory: 57124 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -5,28 +5,22 @@ int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
     int n; cin >> n;
     
-    for(int i=0;i<n;i++){
+    while(n--){
         int k; cin >> k;
 
         priority_queue<int> max_pq;
         priority_queue<int, vector<int>, greater<int>> min_pq;
         multiset<int> ms;
-        
-        for(int j=0;j<k;j++){
-            char c; cin >> c;
-            int num; cin >> num;
+
+        while(k--){
+            char c; cin >> c; int num; cin >> num;
 
             if(c == 'I'){
-                max_pq.push(num);
-                min_pq.push(num);
-                ms.insert(num);
+                max_pq.push(num); min_pq.push(num); ms.insert(num);
             }
             else if(c == 'D'){
-
-                if(ms.empty()) continue;
-
                 if(num == -1){
-                    while(!min_pq.empty() && ms.find(min_pq.top()) == ms.end()) min_pq.pop(); // max delete 할때 안없앤 element(ms에 없는) 없애기.
+                    while(!min_pq.empty() && ms.find(min_pq.top()) == ms.end()) min_pq.pop();
                     if(!min_pq.empty()){
                         ms.erase(ms.find(min_pq.top()));
                         min_pq.pop();
@@ -40,15 +34,15 @@ int main(){
                     }
                 }
             }
-
         }
-        if (ms.empty()) {
-            cout << "EMPTY\n";
-        } else {
-            while (!max_pq.empty() && ms.find(max_pq.top()) == ms.end()) max_pq.pop();
-            while (!min_pq.empty() && ms.find(min_pq.top()) == ms.end()) min_pq.pop();
+
+        if(ms.empty()) cout << "EMPTY\n";
+        else{
+            while(!max_pq.empty() && ms.find(max_pq.top()) == ms.end()) max_pq.pop();
+            while(!min_pq.empty() && ms.find(min_pq.top()) == ms.end()) min_pq.pop();
             cout << max_pq.top() << " " << min_pq.top() << "\n";
         }
     }
+    
     return 0;
 }
