fa45cd2a55b7b237a45483d4d232bb58cef5f3ee
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-08-04 15:44:27 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,49 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int>> vec[20001];
+vector<pair<int,int>> vec[20001];
 int dis[20001];
 
-void dijkstra(int start) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, start});
-    dis[start] = 0;
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int v,e,k; cin >> v >> e >> k;
+    for(int i=0;i<e;i++){
+        int a,b,w; cin >> a >> b >> w;
+        vec[a].push_back({w,b});
+    }
+    fill(dis, dis+20001, INT32_MAX);
+    dis[k] = 0;
 
-    while (!pq.empty()) {
-        int current_dist = pq.top().first;
-        int current_node = pq.top().second;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    pq.push({0, k});
+
+    while(!pq.empty()){
+        int cur_dis = pq.top().first;
+        int cur_node = pq.top().second;
+
         pq.pop();
 
-        if (current_dist > dis[current_node]) continue;
+        if(cur_dis > dis[cur_node]) continue;
 
-        for (auto &edge : vec[current_node]) {
-            int next_node = edge.first;
-            int weight = edge.second;
+        for(auto edge : vec[cur_node]){
+            int weight = edge.first;
+            int next_node = edge.second;
 
-            if (dis[next_node] > dis[current_node] + weight) {
-                dis[next_node] = dis[current_node] + weight;
+            if(dis[next_node] > dis[cur_node] + weight){
+                dis[next_node] = dis[cur_node] + weight;
                 pq.push({dis[next_node], next_node});
             }
         }
     }
-}
 
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    int v, e; cin >> v >> e;
-    int k; cin >> k;
-    fill(dis, dis + 20001, INT32_MAX);
-
-    for (int i = 0; i < e; i++) {
-        int a, b, w; cin >> a >> b >> w;
-        vec[a].push_back({b, w});
-    }
-
-    dijkstra(k);
-
-    for (int i = 1; i <= v; i++) {
-        if (dis[i] == INT32_MAX) cout << "INF\n";
+    for(int i=1;i<=v;i++){
+        if(dis[i] == INT32_MAX) cout << "INF\n";
         else cout << dis[i] << "\n";
     }
+    
+    
     return 0;
 }
