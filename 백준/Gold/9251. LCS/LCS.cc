aa72e72d479ea2c94eaa95b0900453bef5be9994
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-09-09 19:34:31 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5816 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,47 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[1001][1001];
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string s1, s2;
+    cin >> s1 >> s2;
 
-int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    string s1,s2; cin >> s1 >> s2;
+    int dp[s2.size()+1][s1.size()+1];
+    for(int i=0;i<=s1.size();i++) dp[0][i] = 0;
+    for(int i=0;i<=s2.size();i++) dp[i][0] = 0;
 
-    set<int> s[26];
-    for(int i=0;i<s2.size();i++)
-        s[s2[i]-'A'].insert(i);
-    
-    for(auto k : s[s1[0]-'A']){
-        for(int i=k;i<s2.size();i++) arr[0][i] = 1;
-        break;
-    }
-
-    for(int i=1;i<s1.size();i++){
-        for(auto k : s[s1[i] - 'A']){
-            arr[i][k] = arr[i-1][k-1] + 1;
-        }
-        int temp = arr[i][0];
-        for(int j=1;j<s2.size();j++){
-            if(temp < arr[i][j]) temp = arr[i][j];
-            arr[i][j] = temp;
-        }
-        for(int j=0;j<s2.size();j++){
-            arr[i][j] = max(arr[i-1][j] , arr[i][j]);
+    for(int i=1;i<=s2.size();i++){
+        for(int j=1;j<=s1.size();j++){
+            if(s2[i-1] == s1[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
         }
     }
 
-    // for(int i=0;i<s1.size();i++){
-    //     for(int j=0;j<s2.size();j++){
-    //         cout << arr[i][j] << " ";
+
+    cout << dp[s2.size()][s1.size()];
+    // for(int i=0;i<=s2.size();i++){
+    //     for(int j=0;j<=s1.size();j++){
+    //         cout << dp[i][j] << " ";
     //     }
-    //     cout << '\n';
+    //     cout << "\n";
     // }
+    
 
-    int ans = 0;
-    for(int i=0;i<s2.size();i++){
-        ans = max(ans, arr[s1.size()-1][i]);
-    }
-    cout << ans;
+    // int x = s2.size(), y = s1.size();
+
+    // deque<char> deq;
+    // for(;;){
+    //     if(dp[x][y] == 1){
+    //         deq.push_front(s1[y-1]); break;
+    //     }
+
+    //     if(dp[x-1][y] < dp[x][y] && dp[x][y-1] < dp[x][y] && dp[x-1][y-1] < dp[x][y]){
+    //         deq.push_front(s1[y-1]);
+    //         x--,y--;
+    //     }
+    //     else y--;
+    // }
+    // cout << deq.size() << "\n";
+    // for(auto k : deq) cout << k;
     
     return 0;
 }
