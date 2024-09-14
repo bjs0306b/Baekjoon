8eeea6b39a6358a4278c27ab0ea74d92928371a6
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-09-14 22:43:13 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 36 ms, Memory: 10728 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,39 +1,52 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-pair<bool, int> check[1000001];
+vector<int> numbers;
+int scores[1000006];
+int cards[1000006];
 
-int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    int n; cin >> n;
-    int arr[n]; 
-    for(int i=0;i<n;i++){
-        int num; cin >> num;
-        arr[i] = num;
-        check[num].first = true;
-    }
-    for(int i=0;i<n;i++){
-        int temp = arr[i];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-        for(int j=1;j<=sqrt(temp);j++){
-            if(temp % j == 0){
-                if(check[j].first){
-                    check[j].second++;
-                    check[temp].second--;
-                }
-                if(temp != j*j && check[temp/j].first){
-                    check[temp/j].second++;
-                    check[temp].second--;
-                }
-            }
-        }
-    }
+	// 플레이어 수 입력
+	int N;
+	cin >> N;
 
-    for(int i=0;i<n;i++){
-        cout << check[arr[i]].second << " ";
-    }
+	// 각 플레이어의 카드 넘버 입력
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
 
+		numbers.push_back(temp); // 각 플레이어의 카드 넘버 저장
+		cards[temp] = 1; // 특정 플레이어가 해당 카드 넘버를 가지고 있는지 체크
+	}
 
-    
-    return 0;
+	// 에라토스테네스의 체 응용
+	// 각 플레이어의 카드 넘버의 배수를 각각 체크하면서 해당 카드가 있다면, 결투
+	for (int i = 0; i < N; i++)
+	{
+		// 해당 카드 넘버의 배수마다 탐색
+		for (int j = numbers[i] * 2; j < 1000001; j += numbers[i])
+		{
+			// 해당 카드 넘버의 배수의 카드가 존재하면 결투
+			if (cards[j] == 1)
+			{
+				scores[numbers[i]]++;
+				scores[j]--;
+			}
+		}
+	}
+
+	// 각 플레이어의 점수 출력
+	for (int i = 0; i < N; i++)
+		cout << scores[numbers[i]] << " ";
+
+	return 0;
 }
