3f3610ca6652228d95a1249631cc044ab6f5a418
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-09-22 15:20:12 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -1,21 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-struct point
-{
-    ll x, y;
-} p1, p2, p3, p4;
-int ccw(const point &a, const point &b, const point &c)
-{
-    ll ret = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-    if (ret < 0)
-        return -1;
-    else if (ret > 0)
-        return 1;
-    else
-        return 0;
+
+struct point{
+    long long x,y;
+} p1,p2,p3,p4;
+
+int ccw(point a, point b, point c){
+    long long A = b.x - a.x;
+    long long B = b.y - a.y;
+    long long C = c.x - a.x;
+    long long D = c.y - a.y;
+
+    long long temp = A*D - B*C;
+    if(temp > 0) return 1;
+    else if(temp < 0) return -1;
+    else return 0;
 }
-bool isLine(int a, int b, int c, int d)
+
+bool inLine(int a, int b, int c, int d)
 {
     if (a > b) // b가 더 크게
     {
@@ -27,25 +29,21 @@ bool isLine(int a, int b, int c, int d)
     }
     return (a <= d && b >= c) || (c <= b && d >= a); // 좌표의 대소 관계 확인
 }
-bool isCross()
-{
-    // ccw 구하기
-    int z1 = ccw(p1, p2, p3);
-    int z2 = ccw(p1, p2, p4);
-    int z3 = ccw(p3, p4, p1);
-    int z4 = ccw(p3, p4, p2);
 
-    if (z1 * z2 == 0 && z3 * z4 == 0) // 같은 선상에 있는 경우
-    {
-        return isLine(p1.x, p2.x, p3.x, p4.x) && isLine(p1.y, p2.y, p3.y, p4.y);
-    }
-
-    return z1 * z2 <= 0 && z3 * z4 <= 0;
-}
 int main()
 {
-    ios::sync_with_stdio(0), cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y >> p4.x >> p4.y;
-    cout << isCross();
+
+    int f1 = ccw(p1,p2,p3), f2 = ccw(p1,p2,p4), f3 = ccw(p3,p4,p1), f4 = ccw(p3,p4,p2);
+
+    if(f1 * f2 == 0 && f3 * f4 == 0){
+        if(inLine(p1.x,p2.x,p3.x,p4.x) && inLine(p1.y, p2.y, p3.y, p4.y)) cout << 1;
+        else cout << 0;
+    }
+    else if( f1 * f2 <= 0 && f3 * f4 <= 0) cout << 1;
+    else cout << 0;
+
     return 0;
 }
