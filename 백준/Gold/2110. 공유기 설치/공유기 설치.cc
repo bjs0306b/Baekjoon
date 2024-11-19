af5cca41eae299f1f8c103f8f286bf9efc4015a8
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2024-11-19 14:09:53 +0900

    [Gold IV] Title: 공유기 설치, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -5,18 +5,15 @@ int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n,c;
+    int n, c;
     cin >> n >> c;
     vector<int> v(n); for(int i=0;i<n;i++) cin >> v[i];
     sort(v.begin(), v.end());
-
     int l = 0, r = v[n-1];
     int ans;
-
-    while(l <= r){
+    while(l<=r){
         int m = (l+r)/2;
         int cnt = 1;
-
         int s = v[0];
         for(int i=1;i<n;i++){
             if(v[i] - s >= m){
@@ -25,11 +22,11 @@ int main()
             }
         }
         if(cnt >= c){
-            l = m +1;
+            l = m+1;
             ans = m;
         }
         else r = m-1;
-    } 
+    }
     cout << ans;
 
     return 0;
