62d1cdffc42967e551af646474c7898282d95f32
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-01-01 13:31:19 +0900

    [Gold IV] Title: LCS 3, Time: 4 ms, Memory: 6048 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.cc
+++ 백준/Gold/1958. LCS 3/LCS 3.cc
@@ -8,21 +8,18 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     string s1,s2,s3; cin >> s1 >> s2 >> s3;
-    int x= s1.size(), y = s2.size(), z = s3.size();
-
-    for(int i=1;i<=x;i++){
-        for(int j=1;j<=y;j++){
-            for(int k=1;k<=z;k++){
+    
+    for(int i=1;i<=s1.size();i++){
+        for(int j=1;j<=s2.size();j++){
+            for(int k=1;k<=s3.size();k++){
                 if(s1[i-1] == s2[j-1] && s2[j-1] == s3[k-1]){
                     dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
                 }
-                else{
-                    dp[i][j][k] = max(max(dp[i-1][j][k],dp[i][j-1][k]),max(max(dp[i][j][k-1],dp[i-1][j-1][k]),max(dp[i-1][j][k-1],dp[i][j-1][k-1])));
-                }
+                else dp[i][j][k] = max(max(dp[i-1][j][k], dp[i][j-1][k]), dp[i][j][k-1]);
             }
         }
     }
-    cout << dp[x][y][z];
+    cout << dp[s1.size()][s2.size()][s3.size()];
 
     return 0;
 }
