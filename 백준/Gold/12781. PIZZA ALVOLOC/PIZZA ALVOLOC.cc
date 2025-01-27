cfe1933a514d51a3299f04ee8e9051b0522a0a1c
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-01-27 15:56:00 +0900

    [Gold III] Title: PIZZA ALVOLOC, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
+++ 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
@@ -7,11 +7,9 @@ struct dot{
 
 dot d[4];
 
-int ccw(int a, int b, int c){
+long long ccw(int a, int b, int c){
     int x1 = d[b].x - d[a].x, y1 = d[b].y - d[a].y, x2 = d[c].x - d[a].x, y2 = d[c].y - d[a].y;
-    if(x1*y2 - x2*y1 < 0) return 1;
-    else if(x1*y2 - x2*y1 > 0) return -1;
-    else return 0;
+    return x1*y2 - x2*y1;
 }
 
 int main()  
