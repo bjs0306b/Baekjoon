fc708e532b58f83f942cabfcdc7a49550803ae48
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-01-27 15:53:10 +0900

    [Gold III] Title: PIZZA ALVOLOC, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
+++ 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.cc
@@ -1,26 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-typedef struct {
-    int x, y;
-}pii;
- 
-int ccw(pii a, pii b, pii c) {
-    int ans = (b.x - a.x) * (c.y - a.y) - (b.y - a.y)* (c.x - a.x);
-    if (ans < 0) return 1;
-    else if (ans > 0) return -1;
+
+struct dot{
+    int x,y;
+};
+
+dot d[4];
+
+int ccw(int a, int b, int c){
+    int x1 = d[b].x - d[a].x, y1 = d[b].y - d[a].y, x2 = d[c].x - d[a].x, y2 = d[c].y - d[a].y;
+    if(x1*y2 - x2*y1 < 0) return 1;
+    else if(x1*y2 - x2*y1 > 0) return -1;
     else return 0;
 }
- 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
- 
-    pii a, b, c, d;
-    cin >> a.x >> a.y >> b.x >> b.y >> c.x >> c.y >> d.x >> d.y;
+
+int main()  
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    for(int i=0;i<4;i++) cin >> d[i].x >> d[i].y;
+
+    if(ccw(0,1,2)*ccw(0,1,3) < 0) cout << 1;
+    else cout << 0;
     
-    int ans = ccw(a, b, c) * ccw(a, b, d);
-    if (ans < 0) cout << "1";
-    else cout << "0";
- 
+    
     return 0;
 }
