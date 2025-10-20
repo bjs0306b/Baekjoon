2b06afc4d8dd5db3fb2f2a1fc52f8fef191e1cd7
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-10-20 23:46:45 +0900

    [Gold III] Title: 나머지 합, Time: 124 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -8,14 +8,13 @@ int main()
     int n, m;
     long long ans = 0;
     cin >> n >> m;
-    vector<long long> v(n), mod(n), cnt(m, 0);
+    vector<long long> v(n), mod(n+1), cnt(m, 0);
     for (int i = 0; i < n; i++)
         cin >> v[i];
-    mod[0] = v[0] % m;
-    for (int i = 1; i < n; i++)
-        mod[i] = (mod[i - 1] + v[i]) % m;
-    cnt[0]++;
+    mod[0] = 0;
     for (int i = 0; i < n; i++)
+        mod[i+1] = (mod[i] + v[i]) % m;
+    for (int i = 0; i <= n; i++)
         cnt[mod[i]]++;
     for(int i=0;i<m;i++)
         ans += cnt[i]*(cnt[i]-1)/2;
