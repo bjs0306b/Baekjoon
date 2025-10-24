842a8ea8fdb1d3ad747ce6768a4bbefd1b25a541
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-10-24 21:59:36 +0900

    [Gold III] Title: 다리 만들기, Time: 4 ms, Memory: 2888 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -3,6 +3,7 @@ using namespace std;
 
 int n, A[100][100];
 bool visited[100][100];
+set<pair<int, int>> coast; // 해안가
 
 void input()
 {
@@ -15,16 +16,25 @@ void input()
 int dx[] = {-1, 1, 0, 0}, dy[] = {0, 0, -1, 1};
 void assign(int x, int y, int num)
 {
+    bool is_coast = false; // 해안가 체크, 주변에 하나라도 0이면 해안가
+
     A[x][y] = num;
     for (int i = 0; i < 4; i++)
     {
         int nx = x + dx[i], ny = y + dy[i];
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && A[nx][ny] && !visited[nx][ny])
+        if (nx >= 0 && nx < n && ny >= 0 && ny < n)
         {
-            visited[nx][ny] = true;
-            assign(nx, ny, num);
+            if (A[nx][ny] && !visited[nx][ny])
+            {
+                visited[nx][ny] = true;
+                assign(nx, ny, num);
+            }
+            if (A[nx][ny] == 0)
+                is_coast = true;
         }
     }
+    if (is_coast)
+        coast.insert({x, y});
 }
 
 void assign_island_num()
@@ -44,7 +54,7 @@ void assign_island_num()
     }
 }
 
-int check_distance(int x, int y)
+int check_distance(int x, int y, int island_num)
 {
     queue<pair<int, int>> q;
     q.push({x, y});
@@ -58,8 +68,7 @@ int check_distance(int x, int y)
     }
     visited[x][y] = true;
 
-    int first_island_distance = 0, first_island_num = 0;
-    int cnt = 1;
+    int cnt = 0;
     while (!q.empty())
     {
         int size = q.size();
@@ -72,24 +81,14 @@ int check_distance(int x, int y)
                 int nx = front.first + dx[j], ny = front.second + dy[j];
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny])
                 {
-                    if (A[nx][ny]) // 섬 도착.
-                    {
-                        if (first_island_distance) // 이미 다른 섬 찾음
-                        {
-                            if (first_island_num != A[nx][ny])
-                                return first_island_distance + cnt - 1;
-                        }
-                        else
-                        {
-                            first_island_distance = cnt;
-                            first_island_num = A[nx][ny];
-                        }
-                    }
-                    else // 바다
-                    {
-                        q.push({nx, ny});
-                    }
                     visited[nx][ny] = true;
+                    if(!A[nx][ny]){
+                        q.push({nx,ny});
+                    }
+                    if (A[nx][ny] && A[nx][ny] != island_num) // 다른 섬 도착
+                    {
+                        return cnt;
+                    }
                 }
             }
         }
@@ -101,15 +100,11 @@ int check_distance(int x, int y)
 void shortest_bridge()
 {
     int ans = INT32_MAX;
-    for (int i = 0; i < n; i++)
+    for (auto k : coast)
     {
-        for (int j = 0; j < n; j++)
-        {
-            if (!A[i][j])
-            {
-                ans = min(ans, check_distance(i, j));
-            }
-        }
+        int x = k.first, y = k.second;
+        int island_num = A[x][y];
+        ans = min(ans, check_distance(x, y, island_num));
     }
     cout << ans;
 }
@@ -120,7 +115,7 @@ int main()
     cin.tie(0);
     input();
     assign_island_num(); // 각 섬에 번호 할당
-    shortest_bridge(); // 모든 바다에 대해 가장 가까운 섬 2개에 대해 거리 측정하고 더하기
+    shortest_bridge();   // 해안가와 가장 가까운 다른 섬 거리 구하기
 
     return 0;
 }
