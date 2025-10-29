8ce5e790b69b0b1e3dffe4de0debd58069fd2cce
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-10-29 21:34:47 +0900

    [Gold III] Title: 사다리 조작, Time: 1888 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, h;
+bool A[31][10], flag = false;
+
+void input()
+{
+    cin >> n >> m >> h;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        A[a][b] = true;
+    }
+}
+
+bool check()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int cur = i;
+        for (int j = 1; j <= h; j++)
+        {
+            bool f1 = false, f2 = false;
+            if (cur < n && A[j][cur])
+                f1 = true;
+            if (cur > 1 && A[j][cur - 1])
+                f2 = true;
+            if (f1)
+                cur++;
+            if (f2)
+                cur--;
+        }
+        if (i != cur)
+            return false;
+    }
+    return true;
+}
+
+void dfs(int dep, int m)
+{
+    if (flag)
+        return;
+    if (dep == m)
+    {   
+        if (check())
+            flag = true;
+    }
+    else
+    {
+        for (int i = 1; i <= h; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (!A[i][j])
+                {
+                    if (A[i][j - 1] || A[i][j + 1]) // 양옆에 있을 때
+                        continue;
+
+                    A[i][j] = true;
+                    dfs(dep + 1, m);
+                    A[i][j] = false;
+                }
+            }
+        }
+    }
+}
+
+int solve()
+{
+    for(int i=0;i<4;i++){
+        dfs(0, i);
+        if(flag) return i;
+    }
+    if (flag) return 1;
+    
+    return -1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    input();
+    cout << solve();
+    return 0;
+}
