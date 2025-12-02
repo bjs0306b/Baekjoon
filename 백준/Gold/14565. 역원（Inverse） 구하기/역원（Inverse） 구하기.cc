3d3dc4d4a49eb2b15999df10b2c80b6f2689585f
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2025-12-03 04:16:30 +0900

    [Gold II] Title: 역원(Inverse) 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14565. 역원（Inverse） 구하기/역원（Inverse） 구하기.cc
+++ 백준/Gold/14565. 역원（Inverse） 구하기/역원（Inverse） 구하기.cc
@@ -1,41 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long n,a;
+long long ans1, ans2;
 
-vector<long long> v;
-
-long long gcd(long long aa, long long bb){
-    if(aa % bb){
-        v.push_back(aa/bb);
-        return gcd(bb, aa%bb);
+long long gcd(long long a, long long b){
+    if(b == 0){
+        ans1 = 1;
+        ans2 = 1;
+        return a;
     }
-    return bb;
+    long long ret = gcd(b, a%b);
+    long long temp = ans1;
+    ans1 = ans2;
+    ans2 = temp + ans2 * (-1) * (a/b);
+    return ret;
 }
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
 
-    cin >> n >> a;
-
+    long long n, a; cin >> n >> a;
     cout << n-a << " ";
 
-    long long ans1, ans2;
-    if(gcd(n,a) == 1){
-        if(v.size() == 0){
-            cout << 1;
-            return 0;
-        }
-        ans1 = 1, ans2 = -1*v[v.size()-1];
-        if(v.size() > 1){
-            for(int i=v.size()-2;i>=0;i--){
-                long long temp = ans1;
-                ans1 = ans2;
-                ans2 = temp + ans2*(-1)*v[i];
-            }
-        }
-        cout << (ans2%n + n) % n;
-    }
+    if(gcd(n, a) == 1) cout << (ans2 % n + n) % n;
     else cout << -1;
 
     return 0;
