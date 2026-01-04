2abdfe6f929803d9507ec4cb5a672d374f41233e
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2026-01-05 01:15:46 +0900

    [Platinum V] Title: 카드 게임, Time: 700 ms, Memory: 33440 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.cc
+++ 백준/Platinum/16566. 카드 게임/카드 게임.cc
@@ -2,23 +2,37 @@
 using namespace std;
 
 int n,m,k;
-vector<int> a,b;
+vector<int> a,b, parent;
 
 void input(){
     cin >> n >> m >> k;
-    a.resize(m); for(int i=0;i<m;i++) cin >> a[i];
+    a.resize(m); parent.resize(m); 
+    for(int i=0;i<m;i++){cin >> a[i]; parent[i] = i;}
     b.resize(k); for(int i=0;i<k;i++) cin >> b[i];
+}
+
+int find(int x){
+    if(x == parent[x]) return x;
+    return parent[x] = find(parent[x]);
+}
+
+void merge(int x, int y){
+    int xx = find(x), yy = find(y);
+    if(xx > yy) swap(xx, yy);
+    parent[xx] = yy;
 }
 
 void solve(){
     sort(a.begin(), a.end());
-    vector<bool> visited(m, false);
 
     for(int i=0;i<k;i++){
         auto iter = upper_bound(a.begin(), a.end(), b[i]);
-        while(visited[iter - a.begin()]) iter++;
-        visited[iter - a.begin()] = true;
-        cout << *iter << "\n";        
+        int num = iter - a.begin();
+
+        int num_p = find(num);
+        cout << a[num_p] << "\n";
+
+        if(num_p + 1 < m) merge(num_p, num_p + 1);
     }
 }
 
