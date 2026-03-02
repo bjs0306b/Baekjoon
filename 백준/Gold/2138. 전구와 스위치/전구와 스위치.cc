fe7bd76fe8924a427831bd5d91c08a54d925020f
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2026-03-02 18:26:37 +0900

    [Gold IV] Title: 전구와 스위치, Time: 4 ms, Memory: 2244 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.cc
@@ -1,98 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<char> c[2];
 int n;
-vector<bool> s1, s2;
-vector<int> dif;
-void input()
-{
+vector<bool> is_dif;
+
+void input(){
     cin >> n;
-    s1.resize(n);
-    s2.resize(n);
-    dif.resize(n);
-    for (int i = 0; i < n; i++)
-    {
-        char c;
-        cin >> c;
-        if (c == '0')
-            s1[i] = false;
-        else
-            s1[i] = true;
+    for(int i=0;i<2;i++){
+        c[i].resize(n);
+        for(int j=0;j<n;j++) cin >> c[i][j];
     }
-    for (int i = 0; i < n; i++)
-    {
-        char c;
-        cin >> c;
-        if (c == '0')
-            s2[i] = false;
-        else
-            s2[i] = true;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (s1[i] == s2[i])
-            dif[i] = 0;
-        else
-            dif[i] = 1;
+    is_dif.resize(n);
+    for(int i=0;i<n;i++){
+        if(c[0][i] != c[1][i]) is_dif[i] = true;
+        else is_dif[i] = false;
     }
 }
 
-void solve()
-{
+void solve(){
     int ans = INT32_MAX;
-    // 맨 처음꺼 그냥 냅둠.
-    vector<int> temp1(n, 0);
-    int cnt1 = 0;
-    for (int i = 1; i < n - 1; i++)
-    {
-        if (dif[i - 1] % 2 != temp1[i - 1] % 2)
-        {
-            cnt1++;
-            temp1[i - 1]++;
-            temp1[i]++;
-            temp1[i + 1]++;
+    // 처음 꺼 안눌렀음.
+    vector<bool> temp(n, false);
+    int cnt = 0;
+    for(int i=1;i<n-1;i++){
+        // i-1번째 전구는 이제 i번째 스위치를 통해서만 변경이 가능해짐. 따라서 is_dif와 다르면 반드시 눌러야 함.
+        if(is_dif[i-1] != temp[i-1]){ // 누름
+            cnt++;
+            for(int j=-1;j<=1;j++) temp[i+j] = !temp[i+j];
         }
     }
-    if ((dif[n - 2] % 2) == (temp1[n - 2] % 2) && (dif[n - 1] % 2) == (temp1[n - 1] % 2))
-    {
-        ans = min(ans, cnt1);
-    }
-    else if ((dif[n - 2] % 2) != (temp1[n - 2] % 2) && (dif[n - 1] % 2) != (temp1[n - 1] % 2))
-    {
-        ans = min(ans, ++cnt1);
-    }
+    bool flag1 = is_dif[n-2] == temp[n-2], flag2 = is_dif[n-1] == temp[n-1];
+    if(flag1 && flag2) ans = min(ans, cnt);// 마지막 꺼 안눌러도 됨.
+    else if(!flag1 && !flag2) ans = min(ans, cnt+1);// 마지막 꺼 눌러야 됨.
+    // 이외에는 불가능
 
-    // 맨 처음꺼 누름
-    vector<int> temp2(n, 0);
-    temp2[0] = temp2[1] = 1;
-    int cnt2 = 1;
-    for (int i = 1; i < n - 1; i++)
-    {
-        if (dif[i - 1] % 2 != temp2[i - 1] % 2)
-        {
-            cnt2++;
-            temp2[i - 1]++;
-            temp2[i]++;
-            temp2[i + 1]++;
+    // 처음 꺼 눌렀음.
+    temp.assign(n, false);
+    cnt = 1; // 눌러서 1
+    temp[0] = true, temp[1] = true; // 눌러서 0,1 번쨰 전구 바뀜.
+    for(int i=1;i<n-1;i++){
+        // i-1번째 전구는 이제 i번째 스위치를 통해서만 변경이 가능해짐. 따라서 is_dif와 다르면 반드시 눌러야 함.
+        if(is_dif[i-1] != temp[i-1]){ // 누름
+            cnt++;
+            for(int j=-1;j<=1;j++) temp[i+j] = !temp[i+j];
         }
     }
-    if ((dif[n - 2] % 2) == (temp2[n - 2] % 2) && (dif[n - 1] % 2) == (temp2[n - 1] % 2))
-    {
-        ans = min(ans, cnt2);
-    }
-    else if ((dif[n - 2] % 2) != (temp2[n - 2] % 2) && (dif[n - 1] % 2) != (temp2[n - 1] % 2))
-    {
-        ans = min(ans, ++cnt2);
-    }
+    flag1 = is_dif[n-2] == temp[n-2], flag2 = is_dif[n-1] == temp[n-1];
+    if(flag1 && flag2) ans = min(ans, cnt);// 마지막 꺼 안눌러도 됨.
+    else if(!flag1 && !flag2) ans = min(ans, cnt+1);// 마지막 꺼 눌러야 됨.
+    // 이외에는 불가능
 
-    if (ans == INT32_MAX)
-        cout << -1;
-    else
-        cout << ans;
+    if(ans == INT32_MAX) cout << -1; // 두 경우 다 불가능
+    else cout << ans;
 }
 
-int main()
-{
+int main(){
     cin.tie(0)->sync_with_stdio(0);
 
     input();
