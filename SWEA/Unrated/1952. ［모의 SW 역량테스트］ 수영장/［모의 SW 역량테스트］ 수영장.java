ec2108add180de7af77f6c76b33c35b7d954dcd9
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2026-04-29 14:34:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 115 ms, Memory: 30,148 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,46 +1,57 @@
-import java.util.Scanner;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int[] price = new int[4];
-    static int[] month_cnt = new int[13];
-    static int[] dp = new int[13]; // i 월까지의 최소값
-    static Scanner sc = new Scanner(System.in);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int n;
+	static int[] mn, price, month, period;
+	
+	public static void input() throws IOException {
+		mn = new int[13];
+		price = new int[4];
+		month = new int[13];
+		period = new int[]{0,1,3,12};
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<4;i++) price[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=12;i++) {
+			month[i] = Integer.parseInt(st.nextToken());
+			mn[i] = Integer.MAX_VALUE;
+		}
+		mn[0] = 0;
+	}
+	
+	static void dfs(int dep, int sum) {
+		if(dep > 12 || mn[dep] < sum) return;
+		mn[dep] = Math.min(mn[dep], sum);
+		if(dep == 12) return;
+		
+		for(int i=1;i<4;i++) {
+			if(i == 1 ) dfs(dep+period[i], sum + Math.min(price[i], price[0]*month[dep+1]));
+			else dfs(dep+period[i], sum+price[i]);
+		}
+	}
+	
+	public static void solve() {
+		dfs(0, 0);
+		System.out.println(mn[12]);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		for(int i=1;i<=t;i++) {
+			System.out.print("#" + i + " ");
+			input();
+			solve();
+		}
+	}
 
-    static void input() {
-        for(int i = 0; i < 4; i++) price[i] = sc.nextInt();
-        for(int i = 1; i <= 12; i++) month_cnt[i] = sc.nextInt();
-        dp[0] = 0;
-        for(int i = 1; i <= 12; i++) dp[i] = 1000000000;
-    }
-
-    static void print() {
-        for(int i = 1; i <= 12; i++) System.out.print(dp[i] + " ");
-        System.out.println();
-    }
-
-    static void solve(int test_idx) {
-        for(int i = 1; i <= 12; i++) {
-            dp[i] = Math.min(dp[i], dp[i-1] + month_cnt[i] * price[0]); // 일일 이용권
-
-            if(month_cnt[i] > 0) {
-                dp[i] = Math.min(dp[i], dp[i-1] + price[1]); // 월간 이용권
-                if(i > 2) dp[i] = Math.min(dp[i], dp[i-3] + price[2]); // 3개월 이용권
-            } else {
-                dp[i] = Math.min(dp[i], dp[i-1]);
-            }
-        }
-
-        int ans = Math.min(price[3], dp[12]); // 연간 이용권
-        System.out.println("#" + test_idx + " " + ans);
-    }
-
-    public static void main(String[] args) {
-        // C++의 cin.tie(0)->sync_with_stdio(0); 부분은 I/O 최적화 코드이므로,
-        // 자바 직역 시에는 영향을 주지 않아 일반 Scanner 환경으로 구현되었습니다.
-        int t = sc.nextInt();
-        for(int i = 0; i < t; i++) {
-            input();
-            solve(i + 1);
-        }
-    }
 }
