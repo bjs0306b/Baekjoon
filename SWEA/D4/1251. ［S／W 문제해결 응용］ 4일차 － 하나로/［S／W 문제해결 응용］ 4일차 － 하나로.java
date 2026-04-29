9bc74f7c4ae69425d3b46109a89ba7b2cd128002
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2026-04-29 14:12:44 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 215 ms, Memory: 34,152 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,13 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-
+	static int[] parent;
+	
+	static int find(int a) {
+		if(parent[a] == a) return a;
+		return parent[a] = find(parent[a]);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		int t = Integer.parseInt(st.nextToken());
@@ -16,7 +23,12 @@ public class Solution {
 			System.out.print("#" + T + " ");
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
-
+			parent = new int[n];
+			for(int i=0;i<n;i++) parent[i] = i;
+			
+			PriorityQueue<long[]> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[0], o2[0]));
+			
+			
 			long xs[] = new long[n], ys[] = new long[n];
 			
 			st = new StringTokenizer(br.readLine());
@@ -29,40 +41,37 @@ public class Solution {
 				ys[i] = Long.parseLong(st.nextToken());	
 			}
 			
-			
 			st = new StringTokenizer(br.readLine());
 			double d = Double.parseDouble(st.nextToken());
-			double ans = 0;
 			
-			boolean[] visited = new boolean[n];
-			long[] dis = new long[n];
-			for(int i=0;i<n;i++) dis[i] = Long.MAX_VALUE;
-			dis[0] = 0;
-			
-			for(;;){
-				int min_idx = -1;
-				long min = Long.MAX_VALUE;
-				for(int i=0;i<n;i++) {
-					if(visited[i]) continue;
-					if(min > dis[i]) {
-						min = dis[i];
-						min_idx = i;
-					}
-				}
-				if(min_idx == -1) break; // 다 방문
-				
-				ans += min;
-				visited[min_idx] = true;
-				
-				for(int i=0;i<n;i++) {
-					if(visited[i]) continue;
-					dis[i] = Math.min(dis[i], (xs[i] - xs[min_idx])*(xs[i] - xs[min_idx]) + (ys[i] - ys[min_idx])*(ys[i] - ys[min_idx]));
+			for(int i=0;i<n-1;i++) {
+				for(int j=i+1;j<n;j++) {
+					long[] temp = new long[3];
+					temp[0] = (xs[i] - xs[j])*(xs[i] - xs[j]) + (ys[i] - ys[j])*(ys[i] - ys[j]);
+					temp[1] = i;
+					temp[2] = j;
+					pq.add(temp);
 				}
 			}
 			
-			
-			ans *= d;
-
+			int cnt = n-1;
+			double ans = 0;
+			while(cnt > 0) {
+				long[] tp = pq.poll();
+				int p1 = find((int)tp[1]), p2 = find((int)tp[2]);
+				if(p1 == p2) continue;
+				
+				if(p1 > p2) {
+					int temp = p1;
+					p1 = p2;
+					p2 = temp;
+				}
+				
+				parent[p1] = p2;
+				
+				cnt--;
+				ans += d * (double)tp[0];
+			}
 			System.out.println(Math.round(ans));
 		}
 	}
