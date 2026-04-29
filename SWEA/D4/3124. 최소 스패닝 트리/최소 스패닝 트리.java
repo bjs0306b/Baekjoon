c71d23542e9ab30ab0bfab75ab47269d015c53c2
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2026-04-29 14:13:15 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,909 ms, Memory: 172,608 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -9,7 +8,13 @@ public class Solution {
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-
+	static int[] parent;
+	
+	static int find(int a) {
+		if(parent[a] == a) return a;
+		return parent[a] = find(parent[a]);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		int t = Integer.parseInt(st.nextToken());
@@ -19,42 +24,40 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
-			ArrayList<int[]>[] v = new ArrayList[n];
-			for(int i=0;i<n;i++) v[i] = new ArrayList<>();
+			parent = new int[n+1];
+			for(int i=1;i<=n;i++) parent[i] = i;
 			
+			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
 			for(int i=0;i<m;i++) {
+				
 				st = new StringTokenizer(br.readLine());
+	
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
-				a--;
-				b--;
-				v[a].add(new int[]{b,c});
-				v[b].add(new int[]{a,c});
-			}	
+				int[] temp = {c, a, b};
+				
+				pq.add(temp);
+			}
 			
-
+			int cnt = n-1;
 			long ans = 0;
-			
-			boolean[] visited = new boolean[n];
-			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)->Long.compare(o1[1], o2[1]));
-			pq.add(new int[] {0,0});
-			int cnt = n;
 			while(cnt > 0) {
 				int[] tp = pq.poll();
-				if(visited[tp[0]]) continue;
+				int p1 = find(tp[1]), p2 = find(tp[2]);
+				if(p1 == p2) continue;
 				
-				ans += tp[1];
-				cnt--;
-				visited[tp[0]] = true;
-				
-				for(int[] k : v[tp[0]]) {
-					if(!visited[k[0]]) {
-						pq.add(k);
-					}
+				if(p1 > p2) {
+					int temp = p1;
+					p1 = p2;
+					p2 = temp;
 				}
+				
+				parent[p1] = p2;
+				
+				cnt--;
+				ans += (long) tp[0];
 			}
-
 			System.out.println(ans);
 		}
 	}
