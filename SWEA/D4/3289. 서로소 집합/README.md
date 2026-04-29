aeee7b790337c13e7faed7dce3f8332d8eb9bdc3
jiseop bang <144401380+bjs0306b@users.noreply.github.com>
2026-04-29 14:16:13 +0900

    [D4] Title: 서로소 집합, Time: 799 ms, Memory: 104,876 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,15 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 104,876 KB, 시간: 799 ms, 코드길이: 1,350 Bytes
+
+### 제출 일자
+
+2026-04-29 14:16
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[] parent;
+	
+	static int find(int a) {
+		if(parent[a] == a) return a;
+		return parent[a] = find(parent[a]);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		
+		for(int T=1;T<=t;T++) {
+			System.out.print("#" + T + " ");
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			parent = new int[n+1];
+			for(int i=1;i<=n;i++) parent[i] = i;
+			
+			for(int i=0;i<m;i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				if(a == 0) {
+					int p1 = find(b), p2 = find(c);
+					if(p1 > p2) {
+						int temp = p1;
+						p1 = p2;
+						p2 = temp;
+					}
+					parent[p1] = p2;
+				}
+				else if(a == 1) {
+					int p1 = find(b), p2 = find(c);
+					if(p1 == p2) System.out.print(1);
+					else System.out.print(0);
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
